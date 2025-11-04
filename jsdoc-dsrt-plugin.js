afb4bc0abf3d9965e57dc4f8df2d619467220809
Bayuu7 <103884982+Bayuu7@users.noreply.github.com>
2025-11-04 14:22:27 +0700

    Update jsdoc-dsrt-plugin.js

--- jsdoc-dsrt-plugin.js
+++ jsdoc-dsrt-plugin.js
@@ -1,11 +1,20 @@
-{
-  "source": {
-    "include": ["src"],
-    "includePattern": ".js$"
-  },
-  "opts": {
-    "destination": "./docs",
-    "recurse": true
-  },
-  "plugins": ["./jsdoc-dsrt-plugin.js"]
-}
+exports.defineTags = function(dictionary) {
+  dictionary.defineTag("dsrt.module", {
+    onTagged: (doclet, tag) => { doclet.kind = "module"; }
+  });
+  dictionary.defineTag("dsrt.class", {
+    onTagged: (doclet, tag) => { doclet.kind = "class"; }
+  });
+  dictionary.defineTag("dsrt.method", {
+    onTagged: (doclet, tag) => { doclet.kind = "function"; }
+  });
+  dictionary.defineTag("dsrt.param", {
+    onTagged: (doclet, tag) => { doclet.params = doclet.params || []; }
+  });
+  dictionary.defineTag("dsrt.return", {
+    onTagged: (doclet, tag) => { doclet.returns = doclet.returns || []; }
+  });
+  dictionary.defineTag("dsrt.description", {
+    onTagged: (doclet, tag) => { doclet.comment = tag.text; }
+  });
+};
